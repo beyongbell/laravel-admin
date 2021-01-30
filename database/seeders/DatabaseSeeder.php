f3df559f784858f9e7f17f91826677053b700a3a
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-30 22:52:11 +0700

    RolePermission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
             OrderSeeder::class,
-            PermissionSeeder::class
+            PermissionSeeder::class,
+            RolePermissionSeeder::class
         ]);
     }
 }
--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -13,6 +16,40 @@ class RolePermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $permissions = Permission::all();
+
+        $admin = Role::whereName('Admin')->first();
+        foreach($permissions as $permission) {
+            DB::table('role_permission')->insert([
+                'role_id' => $admin->id,
+                'permission_id' => $permission->id
+            ]);
+        }
+
+        $editor = Role::whereName('Editor')->first();
+        foreach($permissions as $permission) {
+            if(!in_array($permission->name, ['edit_roles'])) {
+                DB::table('role_permission')->insert([
+                    'role_id' => $editor->id,
+                    'permission_id' => $permission->id
+                ]);
+            }
+        }
+
+        $viewer = Role::whereName('Viewer')->first();
+        $viewerRoles = [
+            'view_users',
+            'view_roles',
+            'view_products',
+            'view_orders'
+        ];
+        foreach($permissions as $permission) {
+            if(!in_array($permission->name, $viewerRoles)) {
+                DB::table('role_permission')->insert([
+                    'role_id' => $viewer->id,
+                    'permission_id' => $permission->id
+                ]);
+            }
+        }
     }
 }
