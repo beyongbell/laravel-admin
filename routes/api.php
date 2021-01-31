3dabbdffd15083d4db15c404b6001c20af81456b
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-31 21:03:29 +0700

    Permission Controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PermissionController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -13,6 +14,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::get('auth/profile',  [AuthController::class, 'profile']);
     Route::post('auth/update',   [AuthController::class, 'update']);
     Route::post('auth/password', [AuthController::class, 'password']);
+    Route::get('permissions', [PermissionController::class, 'index']);
     Route::apiResource('users', UserController::class);
     Route::apiResource('roles', RoleController::class);
     Route::apiResource('products', ProductController::class);
